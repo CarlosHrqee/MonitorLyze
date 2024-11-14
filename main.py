a5de83bf0d775d2fd86d25928cbe0c28a15d970d
CarlosHrqee <159045240+CarlosHrqee@users.noreply.github.com>
2024-11-14 15:52:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
-from fastapi import FastAPI, Response
-from playwright.async_api import async_playwright
-import os
+from flask import Flask
 
-app = FastAPI()
+app = Flask(__name__)
 
-@app.get("/scrape")
-async def root():
-    async with async_playwright() as p:
-        browser = await p.chromium.connect(os.environ['BROWSER_PLAYWRIGHT_ENDPOINT'])
-
-        context = await browser.new_context()
-        page = await context.new_page()
-
-        await page.set_viewport_size({'width': 1920, 'height': 1080});
-
-        await page.goto('https://railway.app')
-        screenshot_bytes = await page.screenshot()
-
-        await browser.close()
-
-        return Response(content=screenshot_bytes, media_type='image/png')
+@app.route('/')
+def hello():
+    return 'Hello, World!'
